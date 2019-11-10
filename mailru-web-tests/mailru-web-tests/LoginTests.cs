587513edc8648a53ab70eaf9ad0ae338eaa069ab
semenovmxm <semenovmxm@gmail.com>
2019-11-10 22:37:00 +0300

    1

---
+++ mailru-web-tests/mailru-web-tests/LoginTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace auto_web_tests
+{
+    [TestFixture]
+    public class MailruTestCase
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://mail.ru/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void LoginTest()
+        {
+            string inboxPageUrl = "https://e.mail.ru/messages/inbox";           
+            string login = "testtztz";
+            string password = "NtcnNtcn1";
+
+            driver.Navigate().GoToUrl(baseURL);
+            driver.FindElement(By.Id("mailbox:login")).Click();
+            driver.FindElement(By.Id("mailbox:login")).Clear();
+            driver.FindElement(By.Id("mailbox:login")).SendKeys(login);
+            driver.FindElement(By.XPath("//label[@id='mailbox:submit']/input")).Click();
+            driver.FindElement(By.Id("mailbox:password")).Click();
+            driver.FindElement(By.Id("mailbox:password")).Clear();
+            driver.FindElement(By.Id("mailbox:password")).SendKeys(password);
+            driver.FindElement(By.XPath("//label[@id='mailbox:submit']/input")).Click();
+            string currentURL = driver.Url;
+            
+            var isInboxPageUrl = currentURL.StartsWith(inboxPageUrl);            
+            Console.WriteLine("url " + currentURL);
+
+            Assert.IsTrue(isInboxPageUrl);
+            Assert.AreEqual(login + "@mail.ru", driver.FindElement(By.Id("PH_user-email")).Text);
+        }
+    }
+}
---
+++ mailru-web-tests/mailru-web-tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("auto-web-tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("auto-web-tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("75891c1c-e152-4487-badd-32d2e2061dd4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
